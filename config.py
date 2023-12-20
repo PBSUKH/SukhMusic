7705798449a80d8b96337984152eb1e3a4a9f835
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2023-12-20 14:30:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "6898413162").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/badmunda011/AMMusic")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/badmunda98/BADOP")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
